309a0f78d12f306c129445224e6972ab37ca93c2
Viktor <vuonghai1095@gmail.com>
2024-06-12 16:52:04 +0700

    update: outputs

--- main.tf
+++ main.tf
@@ -60,25 +60,3 @@ module "ansible-playbook" {
     module.proxmox-vm
   ]
 }
-
-output "cluster_host" {
-  value = local.cluster_host_map
-}
-
-output "group_list" {
-  value = module.ansible-host.group_list
-}
-
-output "rewrite_host_list" {
-  value = module.ansible-host.rewrite_host_list
-
-}
-
-output "rendered_inventory" {
-  value = module.ansible-host.rendered_inventory
-}
-
-# Debug
-output "current-workspace" {
-  value = terraform.workspace
-}
